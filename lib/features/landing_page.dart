0f7f49855aa80cee17274c8225ff6fcc4e596bcf
Richie Paul Aquino <124502283+paullooll@users.noreply.github.com>
2023-10-24 19:10:39 +0800

    Update landing_page.dart

--- lib/features/landing_page.dart
+++ lib/features/landing_page.dart
@@ -3,31 +3,32 @@ import 'package:flutter/material.dart';
 class LandingPage extends StatelessWidget {
  const LandingPage({super.key});
 
- @override
- Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'lib/features/logo.png',
-              width: 250,
-              height: 250,
-            ),
-            const SizedBox(height: 20),
-            const TextField(
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                hintText: 'Welcome to Ambatu.help',
-                hintStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                border: InputBorder.none,
+ @override Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'lib/features/logo.png',
+                width: 250,
+                height: 250,
               ),
-            ),
-            const SizedBox(height: 20),
-            const CircularProgressIndicator(),
-          ],
+              const SizedBox(height: 20),
+              const TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                 hintText: 'Welcome to Ambatu.help',
+                 hintStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                 border: InputBorder.none,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const CircularProgressIndicator(),
+            ],
+          ),
         ),
       ),
     );
