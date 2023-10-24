56404806c387d9333c9230353b34d4fd5beb2b84
Richie Paul Aquino <124502283+paullooll@users.noreply.github.com>
2023-10-24 18:13:46 +0800

    Update landing_page.dart

--- lib/features/landing_page.dart
+++ lib/features/landing_page.dart
@@ -1,37 +1,35 @@
 import 'package:flutter/material.dart';
 
 class LandingPage extends StatelessWidget {
-  const LandingPage({super.key});
+ const LandingPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
+ @override
+ Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Align(
-            alignment: Alignment.center,
-            child: Image.asset(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
               'lib/features/logo.png',
               width: 250,
               height: 250,
             ),
-          ),
-          SizedBox(height: 20), 
-          Center(
-            child: TextField(
+            const SizedBox(height: 20),
+            const TextField(
               textAlign: TextAlign.center,
               decoration: InputDecoration(
                 hintText: 'Welcome to Ambatu.help',
-                hintStyle: TextStyle(fontSize: 20),
+                hintStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 border: InputBorder.none,
               ),
             ),
-          ),
-          SizedBox(height: 10),
-          CircularProgressIndicator(),
-        ],
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(),
+          ],
+        ),
       ),
     );
-  }
+ }
 }
