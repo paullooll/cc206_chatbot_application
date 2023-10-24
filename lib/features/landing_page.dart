00c83343591a25f11a3a1828eb16a8296804baf3
Richie Paul Aquino <124502283+paullooll@users.noreply.github.com>
2023-10-24 19:53:55 +0800

    Update landing_page.dart

--- lib/features/landing_page.dart
+++ lib/features/landing_page.dart
@@ -5,6 +5,7 @@ class LandingPage extends StatelessWidget {
 
  @override Widget build(BuildContext context) {
     return MaterialApp(
+     debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
         body: Center(
