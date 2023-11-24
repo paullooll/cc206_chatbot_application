870247d15220a5c54982a03297bc6ba6fc7273c3
Richie Paul Aquino <124502283+paullooll@users.noreply.github.com>
2023-11-24 19:39:15 +0800

    Update chatmessage.dart

--- lib/chat/chatmessage.dart
+++ lib/chat/chatmessage.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: file_names
 
-import 'package:ui_chatbot/features/home_page.dart';
+import 'package:cc206_chatbot_application/features/home_page.dart';
 
 class ChatMessage{
   String message;
