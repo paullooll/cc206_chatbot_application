3d45d1220a2a2d575506583963e844d77f34f8f4
Richie Paul Aquino <124502283+paullooll@users.noreply.github.com>
2023-11-24 19:38:30 +0800

    Add files via upload

---
+++ lib/chat/bubble.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:ui_chatbot/features/chat/chatmessage.dart';
+import 'package:ui_chatbot/features/home_page.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class ChatBubble extends StatefulWidget{
+  ChatMessage chatMessage;
+  ChatBubble({super.key, required this.chatMessage});
+  @override
+  _ChatBubbleState createState() => _ChatBubbleState();
+}
+
+class _ChatBubbleState extends State<ChatBubble> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 16,right: 16,top: 10,bottom: 10),
+      child: Align(
+        alignment: (widget.chatMessage.type == MessageType.Receiver?Alignment.topLeft:Alignment.topRight),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: (widget.chatMessage.type  == MessageType.Receiver?Colors.blueGrey:Colors.blue),
+          ),
+          padding: const EdgeInsets.all(16),
+          child: Text(widget.chatMessage.message),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/chat/chatmessage.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: file_names
+
+import 'package:ui_chatbot/features/home_page.dart';
+
+class ChatMessage{
+  String message;
+  MessageType type;
+  ChatMessage({required this.message,required this.type});
+}
