c0fc4a718648acb698e86524a8439c7689d245a4
Richie Paul Aquino <124502283+paullooll@users.noreply.github.com>
2023-12-05 14:25:21 +0800

    Delete lib directory

--- lib/chat/chatmessage.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: file_names
-
-import 'package:cc206_chatbot_application/features/home_page.dart';
-
-class ChatMessage{
-  String message;
-  MessageType type;
-  ChatMessage({required this.message,required this.type});
-}
